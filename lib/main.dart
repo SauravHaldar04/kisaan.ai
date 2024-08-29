f054e5280c10f7cdb196c05b140255c6caa5307f
ShantanuDhar <shantanu170604@gmail.com>
2024-08-29 14:25:25 +0530

    Adding common folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nfc3_overload_oblivion/features/auth/pages/login_page.dart';
 import 'package:nfc3_overload_oblivion/features/home/home_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LoginWidget(),
     );
   }
 }
